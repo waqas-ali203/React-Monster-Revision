f1a1fbbb8ee5958f50788af74d78df911c1c16f2
Waqas Ali <waqasansari4547@gmail.com>
2026-03-25 00:50:54 +0500

    React projects

--- demos-projects/src/App.jsx
+++ demos-projects/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Counter from './components/Counter'
 import Todo from './components/Todo'
+import FormValidation from './components/FormValidation'
 
 const App = () => {
   return (
     <div>
       {/* <Counter /> */}
-      <Todo />
+      {/* <Todo /> */}
+      <FormValidation />
     </div>
   )
 }
---
+++ demos-projects/src/components/FormValidation.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FormValidation = () => {
+  return (
+    <div>
+      <h1>Form validation</h1>
+    </div>
+  )
+}
+
+export default FormValidation
