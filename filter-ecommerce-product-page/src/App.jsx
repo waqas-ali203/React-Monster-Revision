b63e490298cbaa5ae9998afc4bd5ed61b3c08a1b
Waqas Ali <waqasansari4547@gmail.com>
2026-04-04 02:10:27 +0500

    Ecommerce card filter

--- filter-ecommerce-product-page/src/App.jsx
+++ filter-ecommerce-product-page/src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Nav from './Navigation/Nav'
+import Products from './Products/Products'
+import Recommended from './Recommended/Recommended'
+import Sidebar from './Sidebar/Sidebar'
 
 const App = () => {
   return (
     <div>
+      {/* <Sidebar /> */}
       <Nav />
+      {/* <Products /> */}
+      <Recommended />
     </div>
   )
 }
